ed4af515adcfacdf331125d6e2260265053db39e
u20587059 <102990130+u20587059@users.noreply.github.com>
2023-04-19 17:05:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,4 @@ import pandas as pd
 import streamlit as st
 
 st.write("This is my first sentence")
+routes = pd.read_data("C:\Users\skhon\Documents\2023\GIS311\assigments\assignment 2\Data\routes.dat")
