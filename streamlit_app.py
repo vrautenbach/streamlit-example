563b4a426db1cff1aeb04e504e57b3aecbc9f817
u20474882 <131455009+u20474882@users.noreply.github.com>
2023-04-21 13:47:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,6 @@ import pandas as pd
 import streamlit as st
 
 st.write("This is my first sentence")
-routes = pd.read_csv('airlines.dat')
+col_names = ["Airline ID", "Name","Alias","IATA","ICAO","Callsign","Country","Active"]
+routes = pd.read_csv('airlines.dat', names = col_names)
 routes
