6ca7637eaac02cc43b7fc4591123ad87a8eca017
u20474882 <131455009+u20474882@users.noreply.github.com>
2023-04-21 14:05:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,7 @@ airlines = pd.read_csv('airlines.dat', names = col_names)
 airlines
 grouped = airlines.groupby(["Country","Active"]).size()
 grouped
+
+airport_col = ['Airport ID', 'Name', 'City', 'Country', 'IATA', 'ICAO', 'Latitude','Longitude', 'Altitude', 'Time Zone', 'DST', 'Tz db time', 'Type', 'Source']
+airports = pd.read_csv('airports.dat', sep =",", names=airport_col)
+airports
