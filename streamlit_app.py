bad8a03c0d8478422995a75214d28d1a5516828f
u20474882 <131455009+u20474882@users.noreply.github.com>
2023-04-21 13:50:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,7 @@ import streamlit as st
 
 st.write("This is my first sentence")
 col_names = ["Airline ID", "Name","Alias","IATA","ICAO","Callsign","Country","Active"]
-routes = pd.read_csv('airlines.dat', names = col_names)
-routes
+airlines = pd.read_csv('airlines.dat', names = col_names)
+airlines
+grouped = airlines.groupby(["Country","Active"]).size()
+grouped
