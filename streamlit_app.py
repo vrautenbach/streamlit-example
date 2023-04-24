2b14ec0add62635ad17c59a5d0b6863f94de0898
u20474882 <131455009+u20474882@users.noreply.github.com>
2023-04-24 15:20:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ output= grouped.aggregate({'Number of airports':'count'}).reset_index()
 st.bar_chart(output, x="Country", y="Number of airports")
 
 st.table(groupedAirlines).sort_values("Active Airlines")
-st.table(output).sort_values()
+st.table(output)
 latitude = airports['latitude']
 longitude = airports['longitude']
 airport_locations = pd.DataFrame(latitude).join(longitude)
